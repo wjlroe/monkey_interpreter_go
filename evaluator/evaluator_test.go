1933a7fca23e78de910e5241de3fde6d92a78cd8
William Roe <git@wjlr.org.uk>
2018-09-21 09:01:05 +0100

    Page 149
    Without TestClosures

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -120,6 +120,7 @@ func TestReturnStatements(t *testing.T) {
 		{"return 10; 9;", 10},
 		{"return 2 * 5; 9;", 10},
 		{"9; return 2 * 5; 9;", 10},
+		{"if (10 > 1) { return 10; }", 10},
 		{
 			`
 			if (10 > 1) {
@@ -131,6 +132,25 @@ func TestReturnStatements(t *testing.T) {
 			}
 			`,
 			10,
+		},
+		{
+			`
+			let f = fn(x) {
+				return x;
+				x + 10;
+			};
+			f(10);`,
+			10,
+		},
+		{
+			`
+			let f = fn(x) {
+				let result = x + 10;
+				return result;
+				return 10;
+			};
+			f(10);`,
+			20,
 		},
 	}
 
@@ -240,6 +260,24 @@ func TestFunctionObject(t *testing.T) {
 
 	if fn.Body.String() != expectedBody {
 		t.Fatalf("body is not %q. got=%q", expectedBody, fn.Body.String())
+	}
+}
+
+func TestFunctionApplication(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"let identity = fn(x) { x; }; identity(5);", 5},
+		{"let identity = fn(x) { return x; }; identity(5);", 5},
+		{"let double = fn(x) { x * 2; }; double(5);", 10},
+		{"let add = fn(x, y) { x + y; }; add(5, 5);", 10},
+		{"let add = fn(x, y) { x + y; }; add(5 + 5, add(5, 5));", 20},
+		{"fn(x) { x; }(5)", 5},
+	}
+
+	for _, tt := range tests {
+		testIntegerObject(t, testEval(tt.input), tt.expected)
 	}
 }
 
