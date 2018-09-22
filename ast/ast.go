c17b3e6e3215ea1f2dbd32f152b96f0366c96f4e
William Roe <git@wjlr.org.uk>
2018-09-22 20:25:19 +0100

    Page 174

--- ast/ast.go
+++ ast/ast.go
@@ -148,6 +148,28 @@ func (al *ArrayLiteral) String() string {
 	return out.String()
 }
 
+type IndexExpression struct {
+	Token token.Token // The [ token
+	Left  Expression
+	Index Expression
+}
+
+func (ie *IndexExpression) expressionNode() {}
+func (ie *IndexExpression) TokenLiteral() string {
+	return ie.Token.Literal
+}
+func (ie *IndexExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("(")
+	out.WriteString(ie.Left.String())
+	out.WriteString("[")
+	out.WriteString(ie.Index.String())
+	out.WriteString("])")
+
+	return out.String()
+}
+
 type PrefixExpression struct {
 	Token    token.Token // the prefix token, e.g. !
 	Operator string
