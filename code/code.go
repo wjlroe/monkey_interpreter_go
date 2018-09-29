976b22abe65a938337dd52b3cc68ba6441e304c1
William Roe <git@wjlr.org.uk>
2018-09-29 20:06:29 +0100

    Compiler page 52

--- code/code.go
+++ code/code.go
@@ -38,6 +38,8 @@ func (int Instructions) fmtInstruction(def *Definition, operands []int) string {
 	}
 
 	switch operandCount {
+	case 0:
+		return def.Name
 	case 1:
 		return fmt.Sprintf("%s %d", def.Name, operands[0])
 	}
@@ -49,6 +51,7 @@ type Opcode byte
 
 const (
 	OpConstant Opcode = iota
+	OpAdd
 )
 
 type Definition struct {
@@ -58,6 +61,7 @@ type Definition struct {
 
 var definitions = map[Opcode]*Definition{
 	OpConstant: {"OpConstant", []int{2}},
+	OpAdd:      {"OpAdd", []int{}},
 }
 
 func Lookup(op byte) (*Definition, error) {
